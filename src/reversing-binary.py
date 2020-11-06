efbb60ace2e7cab6c167cf85f4cabbf7f141f4c9
Nathan Brown <scottynate05@yahoo.com>
2020-11-05 20:20:40 -0800

    reversing binary setup

---
+++ src/reversing-binary.py
@@ -0,0 +1,16 @@
+# Given an integer, write a function that reverses the bits (in binary) and returns the integer result.
+
+# Examples:
+
+# csReverseIntegerBits(417) -> 267
+# 417 in binary is 110100001. Reversing the binary is 100001011, which is 267 in decimal.
+# csReverseIntegerBits(267) -> 417
+# csReverseIntegerBits(0) -> 0
+# Notes:
+
+# The input integer will not be negative.
+# [execution time limit] 4 seconds (py3)
+
+# [input] integer n
+
+# [output] integer
